da0b9b9b11e6f9c2399daab5b38e67b35ea23460
LOGICXYZ <792077431@qq.com>
2025-06-15 15:22:26 +0800

    remove file

--- System/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_System_FILE H:/Qt/System/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/System.exe)
-set(__QT_DEPLOY_TARGET_System_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_System_RUNTIME_DLLS F:/QtTest/6.7.2/mingw_64/bin/Qt6Widgets.dll;F:/QtTest/6.7.2/mingw_64/bin/Qt6Gui.dll;F:/QtTest/6.7.2/mingw_64/bin/Qt6Core.dll)
